03d457275d01c81b6f9102e480dc5c90ab9d7b98
Aniket Kharose <aniketk8010@gmail.com>
2026-09-06 10:54:26 +0530

    add dialog attendance results

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -11,7 +11,7 @@ from src.components.dialog_share_subject import share_subject_dialog
 from src.components.dialog_add_phaoto import add_photos_dialog
 from src.pipelines.face_pipeline import predict_attendance
 from src.database.config import supabase
-
+from src.components.dialog_attendance_results import  attendance_result_dialog
 def teacher_screen():
 
     style_background_dashboard()
