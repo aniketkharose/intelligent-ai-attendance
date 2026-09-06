81a72f84c1fd70849c4cb52a2a8774dfb1446982
Aniket Kharose <aniketk8010@gmail.com>
2026-09-06 15:01:48 +0530

    remove color from text

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -62,7 +62,6 @@ def style_base_layout():
                 font-size: 3.5rem !important;
                 line-height:1.1 !important;
                 margin-bottom:0rem !important;
-                color: black !important;
             }
                 
 
@@ -71,12 +70,10 @@ def style_base_layout():
                 font-size: 2rem !important;
                 line-height:0.9 !important;
                 margin-bottom:0rem !important;
-                color: black !important;
             }
                 
             h3, h4, p {
                 font-family: 'Outfit', sans-serif; 
-                color: #1F2937 !important;   
             }
                 
 
