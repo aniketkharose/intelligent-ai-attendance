666f131df97746bc5af74019e14a3fb19e6ba0d7
Aniket Kharose <aniketk8010@gmail.com>
2026-09-06 15:32:58 +0530

    change footer design

--- src/components/footer.py
+++ src/components/footer.py
@@ -6,8 +6,8 @@ def footer_home():
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:white;"> Created with ❤️ by </p>  
-        <img src='{logo_url}' style='max-height:35px' />
+        <p style="font-weight:bold; color:white;"> Developed with ❤️ by  </p>  
+        <img src='{logo_url}' style='max-height:45px' />
         </div>
                 
                 """, unsafe_allow_html=True)
@@ -18,8 +18,8 @@ def footer_dashboard():
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:black;"> Created with ❤️ by </p>  
-        <img src='{logo_url}' style='max-height:35px' />
+        <p style="font-weight:bold; color:black;"> Developed with ❤️ by  </p>  
+        <img src='{logo_url}' style='max-height:45px' />
         </div>
                 
                 """, unsafe_allow_html=True)
