0d0f7fc41cd087cb565c8d4bcef6232b5f2773ba
Aniket Kharose <aniketk8010@gmail.com>
2026-09-06 16:41:23 +0530

    final changes with footer adjesment

--- src/components/footer.py
+++ src/components/footer.py
@@ -2,24 +2,24 @@ import streamlit as st
 
 
 def footer_home():
-    logo_url = "https://i.ibb.co/xSfk7NWg/Codex-Image-Sep-6-2026-04-03-27-PM.png"
+    logo_url = "https://i.ibb.co/WNs9K6mK/final-logo.png"
     
     st.markdown(f"""
-        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-items:center">
-        <p style="font-weight:bold; color:white;"> Developed with ❤️ by  </p>  
-        <img src='{logo_url}' style='max-height:45px' />
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-items:center;">
+        <p style="font-weight:bold; color:white;margin:0;"> Developed with ❤️ by  </p>  
+        <img src='{logo_url}' style='max-height:40px'  style='max-height:40px; transform:translateY(-8px);'/>
         </div>
                 
                 """, unsafe_allow_html=True)
 
 
 def footer_dashboard():
-    logo_url = "https://i.ibb.co/xSfk7NWg/Codex-Image-Sep-6-2026-04-03-27-PM.png"
+    logo_url = "https://i.ibb.co/WNs9K6mK/final-logo.png"
     
     st.markdown(f"""
-        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-items:center">
-        <p style="font-weight:bold; color:black;"> Developed with ❤️ by  </p>  
-        <img src='{logo_url}' style='max-height:45px' />
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-items:center;">
+        <p style="font-weight:bold; color:black;margin:0;"> Developed with ❤️ by  </p>  
+        <img src='{logo_url}' style='max-height:40px'  style='max-height:40px; transform:translateY(-8px);' />
         </div>
                 
                 """, unsafe_allow_html=True)
